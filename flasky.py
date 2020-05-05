05fa0afebbc7e276482f0c8ecd9a813cf5da4bf6
ducnha <daoducnha29493@gmail.com>
2020-05-05 22:42:03 +0700

    init project

---
+++ flasky.py
@@ -0,0 +1,16 @@
+import os
+from app import create_app, db
+from app.models import User, Role
+from flask_migrate import Migrate
+
+app = create_app(os.getenv("FLASK_CONFIG") or 'default')
+migrate = Migrate(app, db)
+
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+@app.cli.command()
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
---
+++ set_env.bat
@@ -0,0 +1,2 @@
+set FLASK_APP=flasky
+set FLASK_DEBUG=1
